210238c260afb6d709bde169aeead658927111ed
UlissesJunior <ulissesjunior2005@gmail.com>
2022-04-16 20:12:59 -0300

    Link Error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Contato from "./components/contato.jsx";
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/codebypixel/" element={<App />}>
+      <Route path="https://ulissesjunior.github.io/codebypixel/" element={<App />}>
         <Route index element={<App />} /></Route>
         <Route path="sobre" element={<Sobre />}></Route>
         <Route path="projetos" element={<Projetos />}></Route>
