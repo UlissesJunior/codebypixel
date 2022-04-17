5a19919e42146011516b1681be5f4a6fa42dc927
UlissesJunior <ulissesjunior2005@gmail.com>
2022-04-16 23:50:12 -0300

    Sobre Mim 1

--- src/components/sobre.jsx
+++ src/components/sobre.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import "../styles/app.css";
 import Nav from "./navbar";
+import User from "../img/user.png";
 
 function Sobre() {
   return (
     <>
       <div className="container">
         <Nav />
-        <div className="container-box">
-          <div>
+        <div className="container">
+          <div className="container-sobre">
             <h1>
-              <span>Sobre</span>
+              <span>Sobre Mim</span>
             </h1>
-            <h2>Desenvolvedor Front-End Júnior | Web Designer (UI/UX)</h2>
-            <h2>Em busca de novas tecnologias para explorar</h2>
+            <div className="sobre-inline">
+              <img src={User} alt="Ulisses Junior" />
+              <h2>
+                Olá Dev! <br />Meu nome é Ulisses Junior, eu tenho 16 anos.<br /><br /> Sou um
+                desenvolvedor Front-End em constante evolução, descobri que
+                gostava da área no meu curso atual - Técnico em Automação
+                Industrial - que faço no CTIG-UNESP.<br /><br /> Depois que me interessei
+                pela área, eu “revirei” tudo o que eu achei em busca de
+                conhecimento. Tive a base do Front-End na disciplina de
+                Informática no meu curso técnico, depois disso, fiz o curso
+                “HTML, CSS, e Javascript para desenvolvedores web” da Universty
+                John Hopkins através da plataforma Coursera e busquei o restante
+                por conteúdos nos Youtube e no Instagram.<br /><br /> Atualmente eu faço
+                estágio como desenvolvedor na Alumni Unesp Guaratinguetá. Estou
+                no 3º ano do ensino médio integrado ao técnico, ano que vem eu
+                pretendo entrar na faculdade, para cursar Engenharia de
+                Computação.
+              </h2>
+            </div>
           </div>
         </div>
       </div>
