ce779c6af7c9eeac78a5bad1eb91b10fa7c4f6f7
UlissesJunior <ulissesjunior2005@gmail.com>
2022-04-17 20:01:50 -0300

    Page - Contato

--- src/components/contato.jsx
+++ src/components/contato.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "../styles/app.css";
 import { Link } from "react-router-dom";
 import Nav from "./navbar";
-import Atalho from "../img/atalho.png"
-import Earth from "../img/Earth.png"
+import Atalho from "../img/atalho.png";
+import Earth from "../img/Earth.png";
 
 function Contato() {
   return (
@@ -11,17 +11,50 @@ function Contato() {
       <div className="container">
         <Nav />
         <div className="container-box container-contato">
-          <div><div className="contato-img">
-           <img src={Earth} alt="" srcset="" /> </div>
+          <div>
+            <div className="contato-img">
+              <img src={Earth} alt="" srcset="" />{" "}
+            </div>
             <h1>
               <span>Contato</span>
             </h1>
-            <h1><br /></h1>
-            <h2><span>Currículo:</span> <span>Download</span> </h2><br />
-            <h2><span>Email:</span> codebypixel@gmail.com<img src={Atalho} alt="" srcset="" /></h2><br />
-            <h2><span>Instagram:</span> @codebypixel<img src={Atalho} alt="" srcset="" /></h2><br />
-            <h2><span>Github:</span> UlissesJunior<img src={Atalho} alt="" srcset="" /></h2><br />
-            <h2><span>Linkedin:</span> Ulisses Junior<img src={Atalho} alt="" srcset="" /></h2>
+            <h1>
+              <br />
+            </h1>
+            <h2>
+              <span>Currículo:</span> <span>Download</span>{" "}
+            </h2>
+            <br />
+            <h2>
+              <span>Email:</span> codebypixel@gmail.com
+              <a href="mailto:codebypixel@gmail.com" target="_blank">
+                <img src={Atalho} alt="" srcset="" />
+              </a>
+            </h2>
+            <br />
+            <h2>
+              <span>Instagram:</span> @codebypixel{" "}
+              <a href="https://www.instagram.com/codebypixel/" target="_blank">
+                <img src={Atalho} alt="" srcset="" />
+              </a>
+            </h2>
+            <br />
+            <h2>
+              <span>Github:</span> UlissesJunior
+              <a href="https://github.com/UlissesJunior" target="_blank">
+                <img src={Atalho} alt="" srcset="" />
+              </a>
+            </h2>
+            <br />
+            <h2>
+              <span>Linkedin:</span> Ulisses Junior
+              <a
+                href="https://www.linkedin.com/in/ulisses-junior/"
+                target="_blank"
+              >
+                <img src={Atalho} alt="" srcset="" />
+              </a>
+            </h2>
           </div>
         </div>
       </div>
