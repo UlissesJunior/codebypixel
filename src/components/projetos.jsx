7838cade027e53171f9b60d0599d905054bf4f7d
UlissesJunior <ulissesjunior2005@gmail.com>
2022-06-06 09:52:03 -0300

    loading in projects

--- src/components/projetos.jsx
+++ src/components/projetos.jsx
@@ -8,12 +8,15 @@ function Projetos() {
       <div className="container">
         <Nav />
         <div className="container-box">
+          <div className="home-img">
+            <div className="loading">Loading...</div>
+          </div>
           <div>
             <h1>
               <span>Área de Projetos</span>
             </h1>
-            <h2>Desenvolvedor Front-End Júnior | Web Designer (UI/UX)</h2>
-            <h2>Em busca de novas tecnologias para explorar</h2>
+            {/* <h2>Desenvolvedor Front-End Júnior | Web Designer (UI/UX)</h2>
+            <h2>Em busca de novas tecnologias para explorar</h2> */}
           </div>
         </div>
       </div>
